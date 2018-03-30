016e73db2f79ce4b932b4aca8a81fe806824e4a0
MudOnTire <895157882@qq.com>
2018-03-30 09:04:43 +0800

    修改demo

--- server.js
+++ server.js
@@ -1,42 +1,30 @@
-/*
-    //node native server
-    const http = require("http");
-    const fs = require("fs");
-    const port = process.env.port || 1337;
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-    http.createServer(function (request, response) {
-        response.writeHead(200, {
-            'Content-Type': 'text/html',
-            'Access-Control-Allow-Origin': '*'
+http.createServer(function (req, res) {
+    let name = url.parse(req.url, true).query.name;
+
+    if (name === undefined) {
+        name = 'world';
+    }
+
+    if (name === 'burningbird') {
+        const file = './assets/images/phoenix.jpg';
+        fs.stat(file, function (err, stat) {
+            if (err) {
+                console.error(err);
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end("Sorry, Burningbird isn't around right now \n");
+            } else {
+                const img = fs.readFileSync(file);
+                res.contentType = 'image/png';
+                res.contentLength = stat.size;
+                res.end(img, 'binary');
+            }
         });
-        const read = fs.createReadStream(__dirname + '/index.html');
-        read.pipe(response);
-    }).listen(port);
-*/
-
-/*
-    //express server
-    const http = require('http');
-    const express = require('express');
-    const path = require('path');
-
-    const port = process.env.port || 1337;
-    const app = express();
-
-    app.get('/', function (request, response) {
-        response.sendFile(path.join(__dirname, '/index.html'));
-    });
-
-    app.listen(port);
-*/
-
-const server = require('./server/start');
-const router = require('./server/router');
-const requestHandler = require('./server/handler');
-
-let handler = {};
-handler["/"] = requestHandler.home;
-handler["/show"] = requestHandler.show;
-handler["/upload"] = requestHandler.upload;
-
-server.start(router.route, handler);
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello ' + name + '\n');
+    }
+}).listen(8124);
